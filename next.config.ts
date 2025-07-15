6324506a74728b2a4842e000d172397ec2e2bc23
devopsenqineer <161905616+devopsenqineer@users.noreply.github.com>
2025-07-15 20:56:08 +0200

    refactor

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,4 @@
 import { NextConfig } from 'next';
-import { validateEnvVars } from '@/src/configs/envVars';
-
-// Validate and assign default values to environment variables
-validateEnvVars();
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
